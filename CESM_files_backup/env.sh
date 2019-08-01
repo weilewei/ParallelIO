4edebdd3988d75ae1721c1e05cc959b4961681b9
Weile Wei <lokwei9@gmail.com>
2019-08-01 10:28:36 -0600

    remove backup files

--- CESM_files_backup/env.sh
+++
@@ -1,3 +0,0 @@
-export Z5WRAPPER=/glade/u/home/weile/dev/repos_pr/ParallelIO/bld/src/clib
-export BOOST=~/miniconda3/envs/z5-py36
-export LD_LIBRARY_PATH=/glade/u/home/weile/miniconda3/envs/z5-py36/lib:$LD_LIBRARY_PATH
